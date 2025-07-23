9e3489a35f758eddda4f7758fad0b9bff0ca36f2
Yvan <yvandumas05@gmail.com>
2025-07-23 18:46:55 +0200

    Added functions to display and calculate simple calculs. Chain operations don't work

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Operations functions
 function add(a, b) {
     return a + b;
 }
@@ -30,3 +31,43 @@ function operate(op, a, b) {
             break;
     }
 }
+
+// Display functions
+function displayDigits(number){
+    currentDisplay += number;
+    display.textContent = currentDisplay;
+}
+
+function displayClear() {
+    display.textContent = "0";
+    currentDisplay = "";
+}
+
+function setOperator(op) {
+    operator = op;
+    firstNumber = parseInt(currentDisplay);
+    displayClear();
+}
+
+
+let currentDisplay = "";
+let firstNumber = 0;
+let operator = '';
+
+for (let i = 0; i <= 9; i++) {
+    document.querySelector(`#button${i}`).addEventListener("click", () => displayDigits(i));
+}
+const buttonClear = document.querySelector("#clear");
+const buttonEqual = document.querySelector("#equal");
+const display = document.querySelector("#display");
+const buttonAddition = document.querySelector("#addition");
+const buttonSubtraction = document.querySelector("#subtraction");
+const buttonMultiplication = document.querySelector("#multiplication");
+const buttonDivision = document.querySelector("#division");
+
+buttonClear.addEventListener("click", () => displayClear());
+buttonEqual.addEventListener("click", () => display.textContent = operate(operator, firstNumber, parseInt(currentDisplay)));
+buttonAddition.addEventListener("click", () => setOperator('+'));
+buttonSubtraction.addEventListener("click", () => setOperator('-'));
+buttonMultiplication.addEventListener("click", () => setOperator('*'));
+buttonDivision.addEventListener("click", () => setOperator('/'));
